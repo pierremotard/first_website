1040153b0eb95555e40f90a2aa074f57d2b0d7a1
Pierre <pierre@rouge-cardinal-2.local>
2013-12-18 11:13:44 +0100

    Change le prix en dollar sans troncature

---
+++ script.js
@@ -0,0 +1,25 @@
+var  taux_du_dollar = 1.3749;
+
+$(document).ready(function() {
+
+	$('#price').mouseenter(function() {
+		var prix_en_euro, prix_en_dollar;
+
+		prix_en_euro = $('#price').html().replace(" ","").replace("€","");;
+		console.log(prix_en_euro);
+		prix_en_dollar = prix_en_euro * taux_du_dollar;
+		$('#price').html('$ ' + prix_en_dollar)
+
+	});	
+
+	$('#price').mouseleave(function() {
+		var prix_en_euro, prix_en_dollar;
+
+		prix_en_dollar = $('#price').html().replace(/ /,"").replace("$","");;
+		prix_en_euro = prix_en_dollar / taux_du_dollar;
+		console.log(prix_en_euro);
+		$('#price').html(prix_en_euro + ' €')
+
+	});	
+
+});
