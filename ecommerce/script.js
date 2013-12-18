40a092520504eedb1d319cfc0f25d9a3ae4996ff
simonium <pierre.motard99@gmail.com>
2013-12-18 16:37:54 +0100

    Troncature prix chiffre entier

--- ecommerce/script.js
+++ ecommerce/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 		prix_en_euro = $('#price').html().replace(" ","").replace("€","");;
 		console.log(prix_en_euro);
 		prix_en_dollar = prix_en_euro * taux_du_dollar;
-		$('#price').html('$ ' + prix_en_dollar.toFixed(2))
+		$('#price').html('$ ' + prix_en_dollar.toFixed(0))
 
 	});	
 
@@ -17,7 +17,7 @@ $(document).ready(function() {
 		prix_en_dollar = $('#price').html().replace(/ /,"").replace("$","");;
 		prix_en_euro = prix_en_dollar / taux_du_dollar;
 		console.log(prix_en_euro);
-		$('#price').html(prix_en_euro.toFixed(2) + ' €')
+		$('#price').html(prix_en_euro.toFixed(0) + ' €')
 
 	});
 
